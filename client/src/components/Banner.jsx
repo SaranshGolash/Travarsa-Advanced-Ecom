4e77b64cc605c1fe332c9be3bcbbda5ee1b9ebaf
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2026-07-14 13:01:00 +0530

    Finished creating slideshow for the landing page using Bootstrap carousel

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+function Banner() {
+    const imageStyle = {
+        height: '500px',
+        width: '100%',
+        objectFit: 'cover'
+    };
+
+    return(
+        <div className="banner-main">
+            <Carousel>
+                <Carousel.Item>
+                    <img src="/images/banner1.png" alt="First slide" style={imageStyle} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/images/banner2.png" alt="Second slide" style={imageStyle} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/images/banner3.png" alt="Third slide" style={imageStyle} />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+}
+
+export default Banner;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/NavBar";
+import Banner from "../components/Banner";
 
 function Landing() {
     return(
         <div className="landing-main">
-           <NavBar /> 
+           <NavBar />
+           <Banner />
         </div>
     );
 }
